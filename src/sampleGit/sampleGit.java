b09ac148b9473d06bcd82f50eb8183495f94178f
vimalraj s <vraj97515@gmail.com>
2023-06-30 21:34:03 -0700

    sample

---
+++ src/sampleGit/sampleGit.java
@@ -0,0 +1,18 @@
+package sampleGit;
+
+public class sampleGit {
+	
+
+	public void saving(long savingAmount) {
+		System.out.println("savingAmount is ;" +savingAmount);
+	}
+	
+	public void fixed(long fixedAmount) {
+		System.out.println("fixedAmount is ;" +fixedAmount);
+	}
+	public void deposit(long depositAmount) {
+		System.out.println("depositAmount is ;" +depositAmount);
+	}
+	}
+
+
